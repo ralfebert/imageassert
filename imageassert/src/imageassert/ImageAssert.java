a6c261058c3228e44d73f576360c38b7ae70b762
Ralf Ebert <info@ralfebert.de>
2009-12-20 16:20:32 +0100

    initial implementation

---
+++ imageassert/src/imageassert/ImageAssert.java
@@ -0,0 +1,138 @@
+package imageassert;
+
+import static org.junit.Assert.fail;
+
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.FilenameFilter;
+import java.io.InputStream;
+import java.net.URL;
+import java.util.Arrays;
+
+import javax.imageio.ImageIO;
+
+import org.apache.commons.io.FileUtils;
+import org.apache.commons.io.IOUtils;
+import org.apache.commons.io.filefilter.WildcardFileFilter;
+
+public class ImageAssert {
+
+	private static void assertImageEquals(final File expectedFile, final File actualFile,
+			final BufferedImage expected, BufferedImage actual) {
+		boolean equal = Arrays.equals(extractPixels(expected), extractPixels(actual));
+		if (!equal) {
+			ImageCompareDialog imageCompareDialog = new ImageCompareDialog(expected, actual) {
+				@Override
+				protected void onApply() {
+					System.err.println("Wrote PDF to " + expectedFile);
+					try {
+						FileOutputStream out = new FileOutputStream(expectedFile);
+						IOUtils.copy(new FileInputStream(actualFile), out);
+						out.close();
+					} catch (Exception e) {
+						throw new RuntimeException(e);
+					}
+				}
+
+				@Override
+				protected void onReject() {
+					fail("Expected: " + expectedFile.getName());
+				}
+			};
+			imageCompareDialog.open();
+		}
+	}
+
+	private static int[] extractPixels(BufferedImage image) {
+		return image.getRaster().getPixels(0, 0, image.getWidth(), image.getHeight(), (int[]) null);
+	}
+
+	public static void assertPdfEquals(URL expected, InputStream actual) {
+
+		File tempDir = null;
+		try {
+			tempDir = new File("/tmp/pdfassert");
+			if (tempDir.exists())
+				FileUtils.deleteDirectory(tempDir);
+			tempDir.mkdirs();
+
+			File actualFile = new File(tempDir, "actual.pdf");
+			IOUtils.copy(actual, new FileOutputStream(actualFile));
+			File expectedFile = new File(expected.toURI());
+			IOUtils.copy(new FileInputStream(expectedFile), new FileOutputStream(new File(tempDir,
+					"expected.pdf")));
+
+			// TODO: code duplication
+			// TODO: explicit /opt/local/bin/ under OS X required
+			new ProcessBuilder("pdftk", "actual.pdf", "burst", "output", "actual_%02d.pdf")
+					.directory(tempDir).start().waitFor();
+
+			new ProcessBuilder("pdftk", "expected.pdf", "burst", "output", "expected_%02d.pdf")
+					.directory(tempDir).start().waitFor();
+
+			File[] actualFiles = tempDir.listFiles((FilenameFilter) new WildcardFileFilter(
+					"actual_??.pdf"));
+			File[] expectedFiles = tempDir.listFiles((FilenameFilter) new WildcardFileFilter(
+					"expected_??.pdf"));
+
+			Arrays.sort(actualFiles);
+			Arrays.sort(expectedFiles);
+
+			if (expectedFiles.length < 0)
+				fail("No pages in expected PDF!");
+
+			for (int i = 0; i < expectedFiles.length; i++) {
+				File expectedPageFile = expectedFiles[i];
+				if (actualFiles.length < i) {
+					fail("Actual page has not enough pages!: " + actualFiles.length + " < "
+							+ expectedPageFile.length());
+				}
+
+				File actualPageFile = actualFiles[i];
+
+				File expectedPng = convertPdfToPng(expectedPageFile);
+				File actualPng = convertPdfToPng(actualPageFile);
+
+				// TODO: find a better way to do this
+				File expectedSrcFile = new File(expectedFile.getAbsolutePath().replace(
+						"/target/test-classes/", "/src/test/java/"));
+
+				assertImageEquals(expectedSrcFile, actualFile, ImageIO.read(expectedPng), ImageIO
+						.read(actualPng));
+			}
+
+			if (actualFiles.length != expectedFiles.length) {
+				fail("Page count doesn't match, was: " + actualFiles.length + ", expected "
+						+ expectedFiles.length);
+			}
+
+		} catch (Exception e) {
+			throw new RuntimeException(e);
+		}
+	}
+
+	private static File convertPdfToPng(File pdfFile) {
+		File destFile;
+		try {
+			String src = pdfFile.getAbsolutePath();
+			String dest = src.replaceAll(".pdf$", ".png");
+			// if (new ProcessBuilder("/usr/bin/sips", "--resampleWidth",
+			// "1000", "-s", "format",
+			// "png", src, "--out", dest).start().waitFor() != 0)
+			// TODO: support sips and imagemagick
+			// TODO: split might not be necessary when using imagemagick
+			if (new ProcessBuilder("convert", "-density", "300", src, "-resize", "800x", dest)
+					.start().waitFor() != 0)
+				throw new RuntimeException("convert pdf2png returned error");
+			destFile = new File(dest);
+			if (!destFile.exists())
+				throw new RuntimeException(dest + " not found");
+		} catch (Exception e) {
+			throw new RuntimeException(e);
+		}
+		return destFile;
+	}
+
+}
---
+++ imageassert/src/imageassert/ImageCompareDialog.java
@@ -0,0 +1,115 @@
+package imageassert;
+
+import java.awt.BorderLayout;
+import java.awt.Color;
+import java.awt.Dimension;
+import java.awt.Graphics;
+import java.awt.GraphicsConfiguration;
+import java.awt.GraphicsDevice;
+import java.awt.GraphicsEnvironment;
+import java.awt.Transparency;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.KeyAdapter;
+import java.awt.event.KeyEvent;
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
+import java.awt.image.BufferedImage;
+
+import javax.swing.JButton;
+import javax.swing.JComponent;
+import javax.swing.JDialog;
+import javax.swing.JScrollPane;
+
+public class ImageCompareDialog extends JDialog {
+
+	private BufferedImage currentImage;
+	private boolean applied = false;
+
+	public ImageCompareDialog(final BufferedImage expected, final BufferedImage actual) {
+
+		setTitle("1: expected, 2: Actual");
+
+		GraphicsEnvironment env = GraphicsEnvironment.getLocalGraphicsEnvironment();
+		GraphicsDevice device = env.getDefaultScreenDevice();
+		GraphicsConfiguration config = device.getDefaultConfiguration();
+
+		// Create compatible images for fast scrolling
+		final BufferedImage expectedImage = config.createCompatibleImage(expected.getWidth(),
+				expected.getHeight(), Transparency.TRANSLUCENT);
+		expectedImage.getGraphics().drawImage(expected, 0, 0, null);
+
+		final BufferedImage actualImage = config.createCompatibleImage(expected.getWidth(),
+				expected.getHeight(), Transparency.TRANSLUCENT);
+		actualImage.getGraphics().drawImage(actual, 0, 0, null);
+
+		this.currentImage = expectedImage;
+
+		final JComponent panel = new JComponent() {
+			@Override
+			public void paint(Graphics g) {
+				g.drawImage(currentImage, 0, 0, Color.WHITE, null);
+			}
+		};
+
+		panel.setPreferredSize(new Dimension(expectedImage.getWidth(), expectedImage.getHeight()));
+		JScrollPane scrollPane = new JScrollPane(panel);
+		add(scrollPane);
+
+		JButton applyButton = new JButton("Apply");
+		applyButton.addActionListener(new ActionListener() {
+
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				onApply();
+				applied = true;
+				setVisible(false);
+			}
+
+		});
+		add(applyButton, BorderLayout.PAGE_END);
+
+		addWindowListener(new WindowAdapter() {
+
+			@Override
+			public void windowClosed(WindowEvent e) {
+				if (!applied) {
+					onReject();
+				}
+			}
+		});
+
+		setBackground(Color.WHITE);
+
+		applyButton.addKeyListener(new KeyAdapter() {
+
+			@Override
+			public void keyPressed(KeyEvent e) {
+				if (e.getKeyChar() == '1') {
+					currentImage = expectedImage;
+					ImageCompareDialog.this.repaint();
+				}
+				if (e.getKeyChar() == '2') {
+					currentImage = actualImage;
+					ImageCompareDialog.this.repaint();
+				}
+			}
+
+		});
+		setSize(expectedImage.getWidth() + 30, expectedImage.getHeight());
+	}
+
+	protected void onApply() {
+
+	}
+
+	protected void onReject() {
+
+	}
+
+	public void open() {
+		setModal(true);
+		setVisible(true);
+	}
+
+}
