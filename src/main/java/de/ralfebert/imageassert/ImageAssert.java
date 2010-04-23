4aa50013aaae7d5b33278054613dad7448d5b9e9
Ralf Ebert <info@ralfebert.de>
2010-04-23 20:02:57 +0200

    swing support is back, more javadoc

--- src/main/java/de/ralfebert/imageassert/ImageAssert.java
+++ src/main/java/de/ralfebert/imageassert/ImageAssert.java
@@ -15,11 +15,32 @@ import org.apache.commons.io.IOUtils;
 import de.ralfebert.imageassert.compare.ICompareResultHandler;
 import de.ralfebert.imageassert.compare.PageImage;
 import de.ralfebert.imageassert.compare.junit.JUnitCompareResultHandler;
+import de.ralfebert.imageassert.compare.swing.SwingCompareResultHandler;
+import de.ralfebert.imageassert.compare.swt.SwtCompareResultHandler;
 import de.ralfebert.imageassert.pageimage.IPdfImageSplitter;
 import de.ralfebert.imageassert.pageimage.ImageMagickSplitter;
 import de.ralfebert.imageassert.utils.RuntimeIOException;
 import de.ralfebert.imageassert.utils.TemporaryFolder;
 
+/**
+ * ImageAssert compares two PDF documents in JUnit tests. It creates images for
+ * every page, by default using ImageMagick (see setPdfImageSplitter to
+ * implement your own strategy). The images are compared pixel-by-pixel. A
+ * compare dialog is shown, if SWT is available on the classpath using SWT,
+ * otherwise using Swing. If running with '-Djava.awt.headless=true' no compare
+ * dialog is shown. You can also implement your own way to show the results, see
+ * setCompareResultHandler.
+ * 
+ * Usage example:
+ * 
+ * <pre>
+ * ImageAssert imageAssert = new ImageAssert();
+ * imageAssert.assertPdfEquals(Colocated.toStream(this, &quot;example.pdf&quot;), new FileInputStream(
+ * 		actualPdfFile));
+ * </pre>
+ * 
+ * @author Ralf Ebert
+ */
 public class ImageAssert {
 
 	private ICompareResultHandler compareResultHandler = new JUnitCompareResultHandler();
@@ -31,19 +52,19 @@ public class ImageAssert {
 
 	public ImageAssert(boolean showCompareDialog) {
 		if (showCompareDialog) {
-			showCompareDialog();
+			activateCompareDialog();
 		}
 	}
 
-	private void showCompareDialog() {
+	private void activateCompareDialog() {
+		boolean useSWT = false;
 		try {
-			if (Class.forName("org.eclipse.swt") != null) {
-
-			}
-		} catch (ClassNotFoundException e) {
-			// setCompareResultHandler(new SwingCompareResultHandler());
+			useSWT = (Class.forName("org.eclipse.swt.SWT") != null);
+		} catch (Exception e) {
+			// ignore
 		}
-
+		setCompareResultHandler(useSWT ? new SwtCompareResultHandler()
+				: new SwingCompareResultHandler());
 	}
 
 	private void assertImageEquals(PageImage expectedImage, PageImage actualImage) {
---
+++ src/main/java/de/ralfebert/imageassert/compare/swing/SwingCompareResultHandler.java
@@ -0,0 +1,44 @@
+package de.ralfebert.imageassert.compare.swing;
+
+import static org.junit.Assert.fail;
+
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.util.logging.Logger;
+
+import org.apache.commons.io.IOUtils;
+
+import de.ralfebert.imageassert.compare.ICompareResultHandler;
+import de.ralfebert.imageassert.compare.PageImage;
+
+public class SwingCompareResultHandler implements ICompareResultHandler {
+
+	private static final Logger log = Logger.getLogger(SwingCompareResultHandler.class.getName());
+
+	@Override
+	public void onImageNotEqual(final PageImage expected, final PageImage actual) {
+		ImageCompareDialog imageCompareDialog = new ImageCompareDialog(expected.getImage(), actual
+				.getImage()) {
+
+			@Override
+			protected void onApply() {
+				try {
+					FileOutputStream out = new FileOutputStream(expected.getFile());
+					IOUtils.copy(new FileInputStream(actual.getFile()), out);
+					out.close();
+					log.info("Wrote PDF to " + expected.getName());
+				} catch (Exception e) {
+					throw new RuntimeException(e);
+				}
+			}
+
+			@Override
+			protected void onReject() {
+				fail("Expected: " + expected.getName());
+			}
+
+		};
+		imageCompareDialog.open();
+	}
+
+}
